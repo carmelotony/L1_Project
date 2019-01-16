1c289a408eaf16552dab00e3398c00fde2a4bdd3
carmelotony <33266466+carmelotony@users.noreply.github.com>
2019-01-15 20:56:26 -0500

    Add project files.

---
+++ L1_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L1_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
