b989aa1b31c1fa54d5014ff0711e46c7b7182da4
carmelotony <33266466+carmelotony@users.noreply.github.com>
2019-01-22 15:49:58 -0500

    Version 2

--- L1_Project/Program.cs
+++ L1_Project/Program.cs
@@ -6,7 +6,29 @@ namespace L1_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exercise 2
+            // Input a number (n) from the user and check if it is even or odd.
+
+            // Write your code here
+
+            //Direct user to enter a number
+            Console.WriteLine("Please enter a number");
+            //Read in user input as a string
+            string n = Console.ReadLine();
+            //Cast string variable n as integer variable m
+            int m = int.Parse(n);
+            //Determine if m is even by verifying that modulo divided by 2 equals 0, and print verification
+            //Else return to user number is odd
+            if (m % 2 == 0)
+            {
+                Console.WriteLine("Your number is even.");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd. ");
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
